e66ad868165fa23c1d830e06865ef6a23a614222
flevix.ru@gmail.com <flevix.ru@gmail.com>
2013-11-24 00:59:32 +0400

    center and circumradius triangle added

--- include/cg/primitives/triangle.h
+++ include/cg/primitives/triangle.h
@@ -2,6 +2,7 @@
 
 #include <cg/primitives/point.h>
 #include <cg/primitives/segment.h>
+#include <cg/operations/distance.h>
 
 #include <boost/array.hpp>
 
@@ -23,6 +24,24 @@ namespace cg
       point_2t<Scalar> const &   operator [] (size_t id) const { return pts_[id]; }
 
       segment_2t<Scalar> side(size_t id) const { return segment_2t<Scalar>(pts_[(id + 1) % 3], pts_[(id + 2) % 3]); }
+
+      point_2t<Scalar> center() {
+          point_2t<Scalar> a = pts_[0];
+          point_2t<Scalar> b = pts_[1];
+          point_2t<Scalar> c = pts_[2];
+          Scalar ab = cg::distance(a, b);
+          Scalar bc = cg::distance(b, c);
+          Scalar ac = cg::distance(a, c);
+          Scalar d = ab + bc + ac;
+
+          Scalar x = (a.x * bc + b.x * ac + c.x * ab) / d;
+          Scalar y = (a.y * bc + b.y * ac + c.y * ab) / d;
+          return point_2t<Scalar>(x, y);
+      }
+
+      Scalar circumradius() {
+          return cg::distance(center(), pts_[0]);
+      }
 
    private:
       boost::array<point_2t<Scalar>, 3 > pts_;
