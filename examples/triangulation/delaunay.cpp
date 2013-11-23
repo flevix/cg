5e3401dbb5fa9977dc1f566df2b722b1d67d9c22
flevix.ru@gmail.com <flevix.ru@gmail.com>
2013-11-24 01:47:21 +0400

    double click event added && simple draw line implement

--- examples/triangulation/delaunay.cpp
+++ examples/triangulation/delaunay.cpp
@@ -8,24 +8,40 @@
 
 using cg::point_2f;
 
-
 struct delaunay_viewer : cg::visualization::viewer_adapter
 {
     delaunay_viewer()
     { }
  
     void draw(cg::visualization::drawer_type & drawer) const
-    { }
+    {
+        drawer.set_color(Qt::red);
+
+        for (size_t i = 1; i < pts.size(); i++) {
+            drawer.draw_line(pts[i - 1], pts[i]);
+        }
+    }
  
     void print(cg::visualization::printer_type & p) const
     {
-        p.corner_stream() << "Delaunay triangulation" << cg::visualization::endl;
+        p.corner_stream() << "rigth click: add point" << cg::visualization::endl;
+        p.corner_stream() << "double click: clear" << cg::visualization::endl;
     }
  
     bool on_release(const point_2f & p)
     {
-       return true;
+       pts.push_back(p);
+       return pts.size() > 1;
     }
+
+    bool on_double_click(const cg::point_2f &)
+    {
+        pts.clear();
+        return true;
+    }
+
+private:
+    std::vector<point_2f> pts;
 };
 
 int main(int argc, char ** argv)
