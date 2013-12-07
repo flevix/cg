d2080003aa0218d54b401ba16024c76f56157208
flevix.ru@gmail.com <flevix.ru@gmail.com>
2013-12-07 12:12:21 +0400

    minkowski fix

--- include/cg/minkowski/minkowski.h
+++ include/cg/minkowski/minkowski.h
@@ -7,7 +7,18 @@
 #include <iostream>
 
 namespace cg {
-    template <typename Scalar>
+    template <class Scalar>
+point_2t<Scalar> future_point(common::range_circulator<contour_2t<Scalar> > it1,
+                              common::range_circulator<contour_2t<Scalar> > it2)
+    {
+        if (orientation4(*it1, *boost::next(it1), *it2, *boost::next(it2)) == CG_LEFT) {
+            return *boost::next(it1) + *it2;
+        } else {
+            return *it1 + *boost::next(it2);
+        }
+    }
+
+    template <class Scalar>
     contour_2t<Scalar> get_minkowski_sum(contour_2t<Scalar> const& obstacle,
                                          contour_2t<Scalar> const& robot) {
         common::range_circulator<contour_2t<Scalar> > it_o
@@ -23,15 +34,19 @@ namespace cg {
 
         contour_2t<Scalar> sum;
         size_t max_points = obstacle.size() + robot.size();
-
-        while (sum.size() < max_points) {
-            sum.add_point(*it_o + *it_r);
+        size_t k = 1;
+        sum.add_point(*it_o + *it_r);
+        while (k++ < max_points) {
             if (orientation4(*it_o, *it_o_next, *it_r, *it_r_next) == CG_LEFT) {
                 it_o++;
                 it_o_next++;
             } else {
                 it_r++;
                 it_r_next++;
+            }
+            if (!orientation(sum[sum.size() - 1], *it_o + *it_r,
+                                future_point(it_o, it_r)) == CG_COLLINEAR) {
+                sum.add_point(*it_o + *it_r);
             }
         }
         return sum;
